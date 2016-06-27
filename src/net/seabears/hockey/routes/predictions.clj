012adf65d1da9d1e6dfc96dabbcb75c434d9b74e
Corey Beres <corey.beres@gmail.com>
2016-06-26 21:54:21 -0400

    cors header

--- src/net/seabears/hockey/routes/predictions.clj
+++ src/net/seabears/hockey/routes/predictions.clj
@@ -1,5 +1,6 @@
 (ns net.seabears.hockey.routes.predictions
-  (:require [clj-time.core :refer [date-time]]
+  (:require [liberator.representation :refer [ring-response]]
+            [clj-time.core :refer [date-time]]
             [clj-time.coerce :as coerce]))
 
 (defn home-team []
@@ -10,39 +11,45 @@
   {:name "Carolina Hurricanes"
    :location "Carolina"})
 
-(defn recent [ctx]
-  {:games [
-           {:scheduled (coerce/to-string (date-time 1986 10 14 4 3 27 456))
-            :home {:team (home-team)
-                   :prediction {:score 1
-                                :confidence 0.57}}
-            :away {:team (away-team)
-                   :prediction {:score 2
-                                :confidence 0.45}}}
-           {:scheduled (coerce/to-string (date-time 1986 10 14 4 3 27 456))
-            :home {:team (home-team)
-                   :prediction {:score 3
-                                :confidence 0.57}}
-            :away {:team (away-team)
-                   :prediction {:score 1
-                                :confidence 0.45}}}
-           ]})
-  
 (defn upcoming [ctx]
-  {:games [
-           {:scheduled (coerce/to-string (date-time 1986 10 14 4 3 27 456))
-            :home {:team (home-team)
-                   :outcome {:score 2
-                             :predicted 1}}
-            :away {:team (away-team)
-                   :outcome {:score 3
-                             :predicted 2}}}
-           {:scheduled (coerce/to-string (date-time 1986 10 14 4 3 27 456))
-            :home {:team (home-team)
-                   :outcome {:score 2
-                             :predicted 3}}
-            :away {:team (away-team)
-                   :outcome {:score 4
-                             :predicted 1}}}
-           ]})
+  (ring-response
+    {:games
+     [{:scheduled (coerce/to-string (date-time 1986 10 14 4 3 27 456))
+       :home {:team (home-team)
+              :prediction {:score 1
+                           :confidence 0.57}}
+       :away {:team (away-team)
+              :prediction {:score 2
+                           :confidence 0.45}}}
+      {:scheduled (coerce/to-string (date-time 1986 10 14 4 3 27 456))
+       :home {:team (home-team)
+              :prediction {:score 3
+                           :confidence 0.57}}
+       :away {:team (away-team)
+              :prediction {:score 1
+                           :confidence 0.45}}}]}
+    {:headers {"Access-Control-Allow-Origin" "*"}}))
+  
+(defn recent [ctx]
+  (ring-response
+    {:games
+     [{:scheduled (coerce/to-string (date-time 1986 10 14 4 3 27 456))
+       :home {:team (home-team)
+              :score 2
+              :prediction {:score 1
+                           :confidence 0.57}}
+       :away {:team (away-team)
+              :score 3
+              :prediction {:score 2
+                           :confidence 0.45}}}
+      {:scheduled (coerce/to-string (date-time 1986 10 14 4 3 27 456))
+       :home {:team (home-team)
+              :score 2
+              :prediction {:score 3
+                           :confidence 0.57}}
+       :away {:team (away-team)
+              :score 4
+              :prediction {:score 1
+                           :confidence 0.45}}}]}
+    {:headers {"Access-Control-Allow-Origin" "*"}}))
 
